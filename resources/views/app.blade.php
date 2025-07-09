7ed493be1091ffc051e20daf903a0e70874c79cb
ndnestor <trabajo.nestor.098@gmail.com>
2025-07-09 16:58:25 +0200

    Open Graph Addition

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,39 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Listana') }}</title>
 
-        <!-- Fonts -->
+        {{-- Open Graph para redes sociales --}}
+        <meta property="og:title" content="Listana - Organiza tus compras" />
+        <meta
+            property="og:description"
+            content="Crea y comparte listas de compras con otros usuarios. Gestiona tus productos de forma simple y rápida con Listana."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://listana.ndnestor.com" />
+        <meta
+            property="og:image"
+            content="https://listana.ndnestor.com/assets/images/share-cover.webp"
+        />
+        <meta property="og:site_name" content="Listana" />
+
+         {{-- Twitter Card (opcional)  --}}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Listana - Organiza tus compras" />
+        <meta
+            name="twitter:description"
+            content="Gestiona y comparte listas de compras con facilidad. Solo con Listana."
+        />
+        <meta
+            name="twitter:image"
+            content="https://listana.ndnestor.com/assets/images/share-cover.webp"
+        />
+
+        {{-- Font --}}
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        {{-- Favicon --}}
         <link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
         <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
